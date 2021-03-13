729d96b85c4d57a71559efdc708135ba1e1e7781
H0R15H0 <shohori13@gmail.com>
2021-03-13 21:56:34 +0900

    Headerも追加

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <VAppBar app elevate-on-scroll color="primary">
+      <VToolbarTitle>
+        <VBtn @click="moveToTop()" x-large depressed color="primary black--text">Torimo</VBtn>
+      </VToolbarTitle>
+
+      <VSpacer></VSpacer>
+
+    </VAppBar>
+  </header>
+</template>
+
+<script>
+export default {
+	methods: {
+    moveToTop() {
+      this.$router.push({name: 'Home'}, () => {})
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
