7e5e1e28707cf2a8ab0de30938fc233f49cc10d2
H0R15H0 <shohori13@gmail.com>
2021-03-14 09:52:40 +0900

    ShowTrip周りで使うコンポーネントの追加。

---
+++ src/components/TripCoursesTimeline.vue
@@ -0,0 +1,57 @@
+<template>
+  <VTimeline>
+    <!-- {{courses}} -->
+    <div v-for="(course, i) in courses" :key="i">
+      <!-- {{course}} -->
+      <VTimelineItem small v-if="!course.name">
+        <div v-if="i % 2 == 0">
+          <div>{{course.place}}</div>
+          <!-- <VIcon class="">mdi-car</VIcon> -->
+        </div>
+        <div v-else>
+          {{course.arrival_time}}
+        </div>
+        <template v-slot:opposite>
+          <div style="text-align:center;" v-if="i % 2 == 0">{{course.arrival_time}}</div>
+          <div style="text-align:center;" v-else>
+            <div>{{course.place}}</div>
+            <!-- <VIcon class="">mdi-car</VIcon> -->
+          </div>
+        </template>
+      </VTimelineItem>
+      <VTimelineItem v-else hide-dot>
+        <VIcon>mdi-car</VIcon>
+      </VTimelineItem>
+    </div>
+    <!-- <VTimelineItem v-for="(course, i) in courses" :key="i" small>
+      <div v-if="i % 2 == 0">
+        <div>{{course.place}}</div>
+        <VIcon class="">mdi-car</VIcon>
+      </div>
+      <div v-else>
+        {{course.arrival_time}}
+      </div>
+       <template v-slot:opposite>
+        <div style="text-align:center;" v-if="i % 2 == 0">{{course.arrival_time}}</div>
+        <div style="text-align:center;" v-else>
+          <div>{{course.place}}</div>
+          <VIcon class="">mdi-car</VIcon>
+        </div>
+      </template>
+    </VTimelineItem> -->
+  </VTimeline>
+</template>
+
+<script>
+export default {  
+  props: {
+    courses: {
+      type: Array,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
